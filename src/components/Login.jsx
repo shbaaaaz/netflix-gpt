95e20883b69b0203a3ae3425cdd6440d749d979a
Shahbaz Khan <khan.shahbaz4595@gmail.com>
2024-03-09 20:31:43 +0100

    Feat: Add sign up screen

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,16 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Header from './Header'
 import { Link } from 'react-router-dom'
 
 const Login = () => {
+  const [isSignupForm, setIsSignupForm] = useState(false)
+
+  const toggleSignUpForm = () => {
+    setIsSignupForm((prev) => !prev)
+  }
   return (
     <div className='bg-login-background'>
       <div className='px-6 bg-gradient-to-b from-black py-1'>
         <Header />
         <div className='flex justify-center py-10'>
           <div className='flex flex-col p-8 md:p-20 bg-black/90 md:bg-black/65 w-full md:w-1/3 rounded'>
-            <h1 className='text-white text-4xl font-semibold'>Sign In</h1>
+            <h1 className='text-white text-4xl font-semibold'>
+              {isSignupForm ? 'Sign up' : 'Sign In'}
+            </h1>
             <form className='flex flex-col gap-6 py-10 text-white'>
+              {isSignupForm && (
+                <input
+                  type='text'
+                  placeholder='Full Name'
+                  className='rounded p-4 outline-none bg-gray-900'
+                />
+              )}
               <input
                 type='text'
                 placeholder='Email Address'
@@ -23,24 +37,42 @@ const Login = () => {
               />
               <div className='flex flex-col gap-2 my-6'>
                 <button className='bg-red-600 py-4 rounded text-lg font-semibold'>
-                  Sign In
+                  {isSignupForm ? 'Sign Up' : 'Sign In'}
                 </button>
-                <div className='flex justify-between text-gray-400'>
-                  <div className='flex gap-1'>
-                    <input id='remember' type='checkbox' className='rounded' />
-                    <label htmlFor='remember' className=''>
-                      Remember me
-                    </label>
+                {!isSignupForm && (
+                  <div className='flex justify-between text-gray-400'>
+                    <div className='flex gap-1'>
+                      <input
+                        id='remember'
+                        type='checkbox'
+                        className='rounded'
+                      />
+                      <label htmlFor='remember' className=''>
+                        Remember me
+                      </label>
+                    </div>
+                    <Link>Need help?</Link>
                   </div>
-                  <Link>Need help?</Link>
-                </div>
+                )}
               </div>
               <div></div>
             </form>
             <div className='flex flex-col text-gray-400 gap-4'>
-              <p className='text-xl'>
-                New to Netflix? <Link className='text-white'>Sign up now</Link>
-              </p>
+              {isSignupForm ? (
+                <p className='text-xl'>
+                  Already a user?{' '}
+                  <button className='text-white' onClick={toggleSignUpForm}>
+                    Sign in
+                  </button>
+                </p>
+              ) : (
+                <p className='text-xl'>
+                  New to Netflix?{' '}
+                  <button className='text-white' onClick={toggleSignUpForm}>
+                    Sign up now
+                  </button>
+                </p>
+              )}
               <p className='text-md'>
                 This page is protected by Google reCAPTCHA to ensure you're not
                 a bot{' '}
