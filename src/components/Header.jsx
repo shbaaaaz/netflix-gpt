dbb7005ae3f06dd427ab7cb444ff9efefbd23360
Shahbaz Khan <khan.shahbaz4595@gmail.com>
2024-03-24 16:23:55 +0100

    Add language only on gpt search

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,13 +7,13 @@ import { onAuthStateChanged } from 'firebase/auth'
 import { auth } from '../utils/firebase'
 import { useDispatch } from 'react-redux'
 import { SUPPORTED_LANGUAGES, USER_LOGO } from '../utils/constants'
-import { useGPT, useGPTSlice } from '../hooks/useGPTSlice'
+import { useGPTSlice } from '../hooks/useGPTSlice'
 import { useConfigSlice } from '../hooks/useConfigSlice'
 
 const Header = () => {
   const { user, addUser, removeUser } = useUserSlice()
-  const { toggleGPTSearchView } = useGPTSlice()
-  const { config, changeLanguage } = useConfigSlice()
+  const { gpt, toggleGPTSearchView } = useGPTSlice()
+  const { changeLanguage } = useConfigSlice()
   const { signout } = useAuth()
   const navigate = useNavigate()
   const dispatch = useDispatch()
@@ -41,25 +41,27 @@ const Header = () => {
       <img src={ImgNetflixLogo} alt='Netflix logo' className='w-36 md:w-64' />
       {user && (
         <div className=' flex justify-start gap-3 items-center text-white'>
-          <select
-            onChange={handleLanguageChange}
-            className='bg-transparent outline-none'
-          >
-            {SUPPORTED_LANGUAGES.map((language) => (
-              <option
-                className='bg-black'
-                key={language.identifier}
-                value={language.identifier}
-              >
-                {language.language}
-              </option>
-            ))}
-          </select>
+          {gpt.showGPTSearch && (
+            <select
+              onChange={handleLanguageChange}
+              className='bg-transparent outline-none'
+            >
+              {SUPPORTED_LANGUAGES.map((language) => (
+                <option
+                  className='bg-black'
+                  key={language.identifier}
+                  value={language.identifier}
+                >
+                  {language.language}
+                </option>
+              ))}
+            </select>
+          )}
           <button
             onClick={() => dispatch(toggleGPTSearchView())}
             className='font-bold bg-green-500 rounded px-5 py-2'
           >
-            GPT Search
+            {gpt.showGPTSearch ? 'Return Home' : 'GPT Search'}
           </button>
           <img className='w-10' src={USER_LOGO} alt='user icon' />
           <button onClick={signout} className='text-white font-bold'>
